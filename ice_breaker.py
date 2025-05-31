f2d7e21b987aac061a4a24ddac9c18066fb16325
Sabarinathan M <sabarimmsr8@gmail.com>
2025-05-31 20:37:21 +0530

    Added Pydantic Output Parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,6 +6,7 @@ from agents.profile_summarizer_agent import summarize as summarize_linkedin_prof
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import AzureChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+from output_parsers import summary_parser
 
 from third_parties.linkedin import scrape_linkedin_profile
 
@@ -13,8 +14,33 @@ from third_parties.linkedin import scrape_linkedin_profile
 def ice_break_with(name: str) -> str:
     linkedin_url = linkedin_lookup_agent(name)
     print("Fetched URL:", linkedin_url)
-    summarized_data = summarize_linkedin_profile(linkedin_url)
-    print(summarized_data)
+    
+    linkedin_profile = scrape_linkedin_profile(linkedin_url)
+
+    llm = AzureChatOpenAI(temperature=0, azure_deployment='gpt-4o-mini', api_version='2025-01-01-preview')
+    user_prompt_template = """
+    Given the Linkedin Profile information,
+    {linkedin_profile}
+    Extract the details and give me
+    1. A short summary
+    2. two interesting facts about them
+    \n{format_instructions}
+    """
+
+    prompt_template = PromptTemplate(
+        template=user_prompt_template,
+        input_variables=["linkedin_profile"],
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
+    )
+
+    chain = prompt_template | llm | summary_parser
+
+    print("invoking chain")
+    resp = chain.invoke(
+        input={"linkedin_profile": linkedin_profile}
+    )
+
+    print(resp)
     
 
 ice_break_with("Eden Marco")
---
+++ output_parsers.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, List
+from pydantic import BaseModel, Field
+from langchain_core.output_parsers import PydanticOutputParser
+
+
+class Summary(BaseModel):
+    # Description is required. so that Langchain will use this
+    # to transform into pydantic object.
+    summary: str = Field(description="summary")
+    facts: List[str] = Field(description="interesting facts about them")
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "summary": self.summary,
+            "facts": self.facts
+        }
+    
+summary_parser = PydanticOutputParser(pydantic_object=Summary)
